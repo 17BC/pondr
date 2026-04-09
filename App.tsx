379592903dd451f52b262e51ca76f414bb87ea88
Homer Cerrafon <bjyc17@yahoo.com>
2026-04-08 16:23:39 -1000

    Update App container and splash screen background to #c9efd8
    Change App.tsx container backgroundColor from white to #c9efd8 and update splash backgroundColor from #DDE6DF to #c9efd8 in app.json for consistent theming.

--- App.tsx
+++ App.tsx
@@ -85,7 +85,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#c9efd8',
     alignItems: 'center',
     justifyContent: 'center',
   },
